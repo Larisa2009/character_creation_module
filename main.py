02eaa70223ad2221d68232bb54eb7c62909e05f3
Лариса Желбанова <zhelbanovalarisa@mail.ru>
2022-12-09 17:10:10 +0800

    add working version without classes

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+"""Module documentation. Describes how classes and functions work.
+Placed at the top of the file (starting from the first line).
+"""
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Choose a player and deal damage to the enemy."""
     if char_class == 'warrior':
         return (f'{char_name} '
                 f'нанёс урон противнику равный '
@@ -18,6 +22,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Choose a player and block the opponent."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -27,6 +32,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Choose a player and apply superpowers."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 '«Выносливость {80 + 25}»')
@@ -37,6 +43,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Choose a player and find out your strengths."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +69,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Choose a player and find out his superpowers."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
